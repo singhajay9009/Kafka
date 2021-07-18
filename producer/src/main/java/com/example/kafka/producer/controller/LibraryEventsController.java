352c610c9e487b145f3364de84fb1bd3990ad922
Ajay Singh <ajaysingh@mac.local>
2021-07-18 14:01:07 +0530

    Add LibraryEvent Producer

--- producer/src/main/java/com/example/kafka/producer/controller/LibraryEventsController.java
+++ producer/src/main/java/com/example/kafka/producer/controller/LibraryEventsController.java
@@ -2,6 +2,9 @@ package com.example.kafka.producer.controller;
 
 
 import com.example.kafka.producer.domain.LibraryEvent;
+import com.example.kafka.producer.producer.LibraryEventProducer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,10 +12,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class LibraryEventsController {
 
+    private final LibraryEventProducer libraryEventProducer;
+
     @PostMapping("/v1/libraryevent")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent){
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
+
+        //invoke Kafka Producer
+        libraryEventProducer.sendLibraryEvents(libraryEvent);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
